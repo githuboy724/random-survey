ce4ffc4b98ad26a1c75ce415c63d35ef686ad01b
githuboy724 <150535622+githuboy724@users.noreply.github.com>
2023-11-19 21:01:09 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,12 +8,9 @@
 
 
 const urls = [
-	'https://www.surveycake.com/s/RydMx',
 	'https://www.surveycake.com/s/O4mDX',
 	'https://www.surveycake.com/s/bxKP1',
-	'https://www.surveycake.com/s/3OBKN',
 	'https://www.surveycake.com/s/29a2N',
-	'https://www.surveycake.com/s/d7xDK',
 	'https://www.surveycake.com/s/6LeXy',
 	'https://www.surveycake.com/s/aWrPW'
 ];
